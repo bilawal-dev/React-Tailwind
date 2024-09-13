c8f9e4dd8ebe9c92ccb60c1ec9760ac80d706b60
RIOxINFANTRY <bk4449719@gmail.com>
2024-09-13 14:20:43 +0500

    Added Quote Generator App

---
+++ Quote Generator App/src/App.jsx
@@ -0,0 +1,9 @@
+import QuoteGenerator from "./QuoteGenerator";
+
+export default function App(){
+  return (
+    <main className="w-full h-screen flex items-center bg-[url('assets/images/background11.jpg')] bg-cover bg-center">
+      <QuoteGenerator />
+    </main>
+  )
+}
